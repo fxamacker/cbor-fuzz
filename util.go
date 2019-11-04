ffff6b81ad7f357cbfe61a20aa95870ef096fd32
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2019-11-03 22:23:35 -0600

    Add more files to corpus and rename package
    - Add 2 more RFC 7049 test values to corpus.
    - Rename package `fuzz` to `cbor`.
    - update README.md.

--- util.go
+++ util.go
@@ -3,7 +3,7 @@
 // Copyright 2015 go-fuzz project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-package fuzz
+package cbor
 
 import (
 	"math"
